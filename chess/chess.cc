fa0f013b72e22f51466eff363505719a67d6daee
gaurmand <gaurav_amwan@hotmail.com>
2020-02-09 10:30:28 -0800

    Change how Chess.printBoard() displays the board
    Use uppercase and lowercase to differentiate between black and white pieces,
    instead of special symbols.
    
    Add vertical bars betweene each entry in each row
    
    Display ranks and files to the left and at the top of the board.

--- chess/chess.cc
+++ chess/chess.cc
@@ -7,49 +7,43 @@ namespace bg{
 Chess::Chess() : currentTurn(0), currentPlayer(WHITE), chessBoard(&chessSet){}
 
 void Chess::printBoard(){
-  std::cout << "Chess board:\n" << std::endl;
+  std::cout << std::endl << " |a|b|c|d|e|f|g|h|" << std::endl; //print files
+  std::cout << "------------------" << std::endl; 
+
   for(uint32_t i=0; i<ChessBoard::NUM_ROWS; i++){
+    std::cout <<  8-i << "|"; //print rank
+
     for(uint32_t j=0; j<ChessBoard::NUM_COLUMNS; j++){
       ChessPiece* piece = chessBoard.getPiece({i, j});
       if (piece == nullptr){
-        std::cout << "[]";
+        std::cout << ".|";
         continue;
       }
 
       switch(piece->type){
         case PAWN:
-          std::cout << "P";
+          std::cout << (piece->player==BLACK ? "p" : "P");
           break;
         case ROOK:
-          std::cout << "R";
+          std::cout << (piece->player==BLACK ? "r" : "R");
           break;
         case KNIGHT:
-          std::cout << "K";
+          std::cout << (piece->player==BLACK ? "n" : "N");
           break;
         case BISHOP:
-          std::cout << "B";
+          std::cout << (piece->player==BLACK ? "b" : "B");
           break;
         case QUEEN:
-          std::cout << "Q";
+          std::cout << (piece->player==BLACK ? "q" : "Q");
           break;
         case KING:
-          std::cout << "+";
+          std::cout << (piece->player==BLACK ? "k" : "K");
           break;
         default:
           std:: cout << "ERROR" << std::endl;
           return;
       }
-      switch(piece->player){
-        case BLACK:
-          std::cout << "*";
-          break;
-        case WHITE:
-          std::cout << "`";
-          break;
-        default:
-          std:: cout << "ERROR" << std::endl;
-          return;
-      }
+      std::cout << "|";
     }
     std::cout << std::endl;
   }
